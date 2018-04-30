16762862087160d2afe2cbf9309eedf877cb5917
ugtony <ugtony@yahoo.com.tw>
2018-04-30 14:21:44 +0800

    initial version

---
+++ iot-face-api.py
@@ -0,0 +1,79 @@
+import requests
+import json
+import base64
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--api_key', help='the key for accessing APIs', default="3Opabtz5OBXZzZk9aGWe")
+parser.add_argument('--server_url', type=str,
+    help='The server url (port included)', default='https://iot.cht.com.tw/api/face')
+parser.add_argument('--http_proxy', type=str,
+    help='http proxy', default=None)
+parser.add_argument('--https_proxy', type=str,
+    help='https proxy', default=None)
+
+args = parser.parse_args(sys.argv[1:])
+server = args.server_url
+proxies = {
+  "http": args.http_proxy,
+  "https": args.https_proxy,
+}
+
+
+# read all images and get the corresponding base64-encoded data
+img_names = ['o-AUDREY-1', 'o-AUDREY-2', 'o-AUDREY-3', 'friends']
+files = {name: open("images/{}.jpg".format(name), "rb") for name in img_names}
+base64_data = {name: base64.b64encode(files[name].read()).decode('utf-8') for name in img_names}
+
+
+###
+#test /v0.2/VerifyFaceImage
+###
+print('** testing /v0.2/VerifyFaceImage **')
+
+url = "{}{}".format(server, '/v0.2/VerifyFaceImage')
+headers = {'Api-Key': args.api_key}
+qdata = base64_data['o-AUDREY-1']
+cdata = [base64_data[v] for v in ['o-AUDREY-1', 'o-AUDREY-2', 'o-AUDREY-3']]
+data = {    
+    "queryData":qdata,
+    "candidateDataArray":cdata     
+}
+response = requests.post(url.format(server), headers = headers, data=json.dumps(data), proxies=proxies)
+print('response:')
+print(response.text)
+print('')
+
+
+###
+#test /v0.2/Cache/Detect
+###
+print('** testing /v0.2/Cache/Detect **')
+url = "{}{}".format(server, '/v0.2/Cache/Detect')
+data = {        
+    "imgData":base64_data['friends'],
+}
+response = requests.post(url.format(server), headers = headers, data=json.dumps(data), proxies=proxies)
+print('response:')
+print(response.text)
+print('')
+
+rdata = json.loads(response.text)
+cacheFaceIds = [face['cacheFaceId'] for face in rdata['faces']] # get cacheFaceIds for further testing
+
+
+
+###
+#test /v0.2/Cache/Match
+###
+print('** testing /v0.2/Cache/Match **')
+url = "{}{}".format(server, '/v0.2/Cache/Match')
+data = {                
+            "queryCacheFaceIds":cacheFaceIds[2:4],    #take the second largest face as the query face
+            "candidateCacheFaceIds":cacheFaceIds[:]    #take all faces as the candidate faces
+        }
+response = requests.post(url.format(server), headers = headers, data=json.dumps(data), proxies=proxies)
+print('response:')
+print(response.text)
+print('')
